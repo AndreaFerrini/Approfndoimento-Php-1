0411529ba86d98baa0da09d83f20d7160259057d
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-10-19 11:55:22 +0200

    test iniziali

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Approfondimento 1</title>
+    </head>
+    <body>
+        <?php
+
+        /*
+            //stampa a video
+            echo "Hello World";
+
+        */
+
+        /*
+            esempio di variabile
+            $anni = 33;
+            echo $anni;
+        */
+
+        ?>
+    </body>
+</html>
